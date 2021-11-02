354434c62db5b21bf3e3579397175d21fa61cb1f
Caleb Hatch <hatchcaleb@gmail.com>
2021-11-02 15:53:44 -0500

    Finished basic code

--- client.js
+++ client.js
@@ -30,6 +30,8 @@ const employees = [
     reviewRating: 1
   }
 ];
+console.log(employees[4].annualSalary.length);
+
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 // This problem is massive! Break the problem down, take small steps, and test as you go.
@@ -46,41 +48,75 @@ for (let employee of employees) {
 // Create function with one object parameter
 function getBonus(employee){
   // return new object with listed properties
+  parseInt(employees.employeeNumber);
+  parseInt(employees.annualSalary);
   let singleEmployee = ratingToBonus(employee);
   return singleEmployee;
 };// End getBonus
 
 // Create math function to determine rating to bonus percentage
 function ratingToBonus(employeeObject){
+  let bonusPercentage = 0;
   let bonusCalculation = 0;
   let singleEmployee = {name: employeeObject.name};
   if(employeeObject.reviewRating <= 2){
     singleEmployee.bonusPercentage = '0%';
-    singleEmployee.totalCompensation = employeeObject.annualSalary;
+    bonusPercentage = 0;
+    // singleEmployee.totalCompensation = employeeObject.annualSalary;
     alert('No bonus for you!')
 
   } else if(employeeObject.reviewRating === 3){
       singleEmployee.bonusPercentage = '4%';
       //4% base annual income as bonus
-      bonusCalculation = employeeObject.annualSalary*.04;
-      singleEmployee.totalCompensation = employeeObject.annualSalary + bonusCalculation;
+      bonusPercentage = .04;
+      //bonusCalculation = employeeObject.annualSalary*.04;
+      //singleEmployee.totalCompensation = employeeObject.annualSalary + bonusCalculation;
 
   } else if(employeeObject.reviewRating === 4){
       singleEmployee.bonusPercentage = '6%';
-      bonusCalculation = employeeObject.annualSalary*.06;
+      bonusPercentage = .06;
+      //bonusCalculation = employeeObject.annualSalary*.06;
       //singleEmployee.totalCompensation = employeeObject.annualSalary + bonusCalculation;
       //6% base annual income as bonus
 
   } else if(employeeObject.reviewRating === 5){
       singleEmployee.bonusPercentage = '10%';
-      bonusCalculation = employeeObject.annualSalary*.1;
+      bonusPercentage = .10;
+      //bonusCalculation = employeeObject.annualSalary*.1;
       //singleEmployee.totalCompensation = employeeObject.annualSalary + bonusCalculation;
       //10% base annual income as bonus
   };
-  
-  if(salary > 65000 && ){
-    bonusCalculation -= .09;
+  //check
+  if(employeeObject.annualSalary > '65000'){
+    //console.log('bonusPercentage before annualSalary check:', bonusPercentage);
+    bonusPercentage -= .01;
+    //console.log('bonusPercentage after annualSalary check:', bonusPercentage);
+  };
+  //check employee number length. If 4 add 5% bonus.
+  if(employeeObject.employeeNumber.length === 4){
+    //console.log('bonusPercentage before employeeNumber', bonusPercentage);
+    bonusPercentage += .05;
+    //console.log('bonusPercentage after employeeNumber', bonusPercentage);
+  };
+
+  // check if bonus percentage is greater than 13, if so set it to 13.
+  if(bonusPercentage > .13){
+    bonusPercentage = .13;
   }
+  //check if bonus percentage is less than 0, if so set it to 0.
+  if(bonusPercentage < 0){
+    bonusPercentage = 0;
+  }
+  //console.log('bonusPercentage after all the checks:', bonusPercentage);
+  //calculate bonusCalculation after all checks and stuff
+  bonusCalculation = Number(employeeObject.annualSalary) * bonusPercentage;
+  //console.log('bonusCalculation before math.round:', bonusCalculation);
+  bonusCalculation = Math.round(bonusCalculation);
+  //console.log('bonusCalculation after math.round:', bonusCalculation);
+  singleEmployee.totalBonus = bonusCalculation;
+  // calculate totalCompensation
+  singleEmployee.totalCompensation = Number(employeeObject.annualSalary) + bonusCalculation;
+  
   return singleEmployee;
 };// end ratingToBonus
 console.log('trial run', getBonus(employees[0]));
