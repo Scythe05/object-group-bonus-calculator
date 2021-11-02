12ebc86a7a19f2a8c58087d7d1d237ee5240a1a2
Caleb Hatch <hatchcaleb@gmail.com>
2021-11-02 15:20:54 -0500

    Mostly working full function

--- client.js
+++ client.js
@@ -46,23 +46,55 @@ for (let employee of employees) {
 // Create function with one object parameter
 function getBonus(employee){
   // return new object with listed properties
-  let singleEmployee = {
-    name: employee.name,
-  };
-
-  ratingToBonus(employee);
+  let singleEmployee = ratingToBonus(employee);
   return singleEmployee;
 };// End getBonus
 
 // Create math function to determine rating to bonus percentage
 function ratingToBonus(employeeObject){
+  let bonusCalculation = 0;
+  let singleEmployee = {name: employeeObject.name};
   if(employeeObject.reviewRating <= 2){
+    singleEmployee.bonusPercentage = '0%';
+    singleEmployee.totalCompensation = employeeObject.annualSalary;
     alert('No bonus for you!')
+
+  } else if(employeeObject.reviewRating === 3){
+      singleEmployee.bonusPercentage = '4%';
+      //4% base annual income as bonus
+      bonusCalculation = employeeObject.annualSalary*.04;
+      singleEmployee.totalCompensation = employeeObject.annualSalary + bonusCalculation;
+
+  } else if(employeeObject.reviewRating === 4){
+      singleEmployee.bonusPercentage = '6%';
+      bonusCalculation = employeeObject.annualSalary*.06;
+      //singleEmployee.totalCompensation = employeeObject.annualSalary + bonusCalculation;
+      //6% base annual income as bonus
+
+  } else if(employeeObject.reviewRating === 5){
+      singleEmployee.bonusPercentage = '10%';
+      bonusCalculation = employeeObject.annualSalary*.1;
+      //singleEmployee.totalCompensation = employeeObject.annualSalary + bonusCalculation;
+      //10% base annual income as bonus
+  };
+  
+  if(salary > 65000 && ){
+    bonusCalculation -= .09;
   }
+  return singleEmployee;
 };// end ratingToBonus
+console.log('trial run', getBonus(employees[0]));
 console.log('trial run', getBonus(employees[3]));
+console.log('trial run', getBonus(employees[1]));
+console.log('trial run', getBonus(employees[2]));
+console.log('trial run', getBonus(employees[4]));
+
+
+
+
+
 
 
 // getBonus(employees[0])
 
-console.log( employees );
+//console.log( employees );
